0e9fad979f35c3e943eb374b44e7ce8ec696bf45
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-02 13:17:51 +0530

    updates

--- src/components/app-container/Container.js
+++ src/components/app-container/Container.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import "./Container.css";
 
-class Container extends Component {
-  render() {
-    return (
-        <div className="container">
+// class Container extends Component {
+//   render() {
+//     return (
+//         <div className="container">
 
-      </div>
-    );
-  }
-}
+//       </div>
+//     );
+//   }
+// }
+let Container = () => {
+   return <div className="container" />;
+};
 
 export default Container;
