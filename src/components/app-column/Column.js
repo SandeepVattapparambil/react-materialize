280c11f6d9a290941896b136246dc4f1fca13cb4
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-04 21:15:44 +0530

    col

---
+++ src/components/app-column/Column.js
@@ -0,0 +1,58 @@
+/**
+ * Component - Column
+ * MaterializeCSS static Column component
+ * @see {@link https://materializecss.com/grid.html | Grid}
+ */
+
+/**
+ * Import react library
+ */
+import React, { Component } from "react";
+
+/**
+ * @class
+ * @name Column
+ * @description MaterializeCSS Column.
+ * @extends Component
+ */
+
+class Column extends Component {
+   constructor(props) {
+      super(props);
+      let config = {
+         ...props
+      };
+      this.state = {
+         options: {
+            small: config.small || " ",
+            medium: config.medium || " ",
+            large: config.large || " ",
+            offset: config.offset || []
+         }
+      };
+   }
+
+   componentDidMount() {
+      if (this._reactInternalFiber.return.stateNode.className !== "row") {
+         console.warn(
+            "Remember when you are creating your layout that all columns must be contained in" +
+               "side a row component."
+         );
+      }
+   }
+
+   render() {
+      let stringArray = [];
+      this.state.options.offset.map(col => {
+         stringArray.push(`offset-${col}`);
+      });
+      let offsetString = stringArray.join(" ");
+      return (
+         <div className={`col ${this.state.options.small} ${offsetString}`}>
+            {this.props.children}
+         </div>
+      );
+   }
+}
+
+export default Column;
