b7926917ba152e9b5b6149857d2829fc87386c8f
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-04 21:31:50 +0530

    divider component

---
+++ src/components/app-divider/Divider.js
@@ -0,0 +1,30 @@
+/**
+ * Component - Divider
+ * MaterializeCSS static Divider component
+ * @see {@link https://materializecss.com/grid.html | Grid - Divider}
+ */
+
+/**
+ * Import react library
+ */
+import React, {Component} from "react";
+
+/**
+ * @class
+ * @name Divider
+ * @description MaterializeCSS Divider Component.
+ * @extends Component
+ */
+class Divider extends Component {
+
+    /**
+     * Static Divider component
+     * Can be reduced to a functional component
+     */
+    render() {
+        return (
+            <div className="divider"></div>
+        );
+    }
+}
+export default Divider;
