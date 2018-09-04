a2a38d40ce5e3f4cdcfd6a6227c34e6714c79092
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-04 21:32:11 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ import Row from "./components/app-row/Row";
 import Container from "./components/app-container/Container";
 import Badge from "./components/app-badge/Badge";
 import Col from "./components/app-column/Column";
+import Divider from "./components/app-divider/Divider";
 
 /**
  * Import service worker
@@ -35,35 +36,49 @@ import registerServiceWorker from "./registerServiceWorker";
  * Render component on to DOM
  */
 ReactDOM.render(
-    <Navbar
-    logo="React Material"
-    fixed="true"
-    color="grey darken-3"
-    textColor="red-text tex-lighten-1"
-    centerLogo="false">
-    <NavbarLinks
-        align="right"
-        list={[
-        {
-            name: "Sass",
-            link: "sass.html",
-            active: true
-        }, {
-            name: "Badges",
-            link: "badges.html"
-        }
-    ]}/>
-</Navbar>, document.getElementById("header"));
+   <Navbar
+      logo="React Material"
+      fixed="true"
+      color="grey darken-3"
+      textColor="red-text tex-lighten-1"
+      centerLogo="false"
+   >
+      <NavbarLinks
+         align="right"
+         list={[
+            {
+               name: "Sass",
+               link: "sass.html",
+               active: true
+            },
+            {
+               name: "Badges",
+               link: "badges.html"
+            }
+         ]}
+      />
+   </Navbar>,
+   document.getElementById("header")
+);
 
 ReactDOM.render(
-    <Container>
-    <Row>
-        <Col small="s6" medium="m6" large="l6" offset={["s2", "m3", "l1"]} push="s1" pull="s3">
-            <Badge type="new" data="1" color="lime"/>
-        </Col>
-
-    </Row>
-</Container>, document.getElementById("container"));
+   <Container>
+      <Row>
+         <Col
+            small="s6"
+            medium="m6"
+            large="l6"
+            offset={["s2", "m3", "l1"]}
+            push="s1"
+            pull="s3"
+         >
+            <Badge type="new" data="1" color="lime" />
+            <Divider/>
+         </Col>
+      </Row>
+   </Container>,
+   document.getElementById("container")
+);
 
 /**
  * Register service worker
