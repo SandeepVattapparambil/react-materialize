d20c7bac89ac7bcc27fd1be1678f2abfefe40e2a
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-06 11:17:50 +0530

    btn wip

--- src/components/app-button/Button.js
+++ src/components/app-button/Button.js
@@ -29,13 +29,14 @@ class Button extends Component {
                 label: config.label || " ",
                 disabled: config.disabled || false,
                 color: config.color || " ",
-                textColor: config.color || "white-text",
+                textColor: config.textColor || "white-text",
                 wavesEffect: config.wavesEffect || true,
                 wavesType: config.wavesType || " ",
                 wavesColor: config.config || " "
             }
         };
     }
+
     render() {
         let btnType;
         if (this.state.options.type === 'raised') {
@@ -44,10 +45,24 @@ class Button extends Component {
             btnType = "btn-floating";
         } else if (this.state.options.type === 'flat') {
             btnType = "btn-flat";
+        } else {
+            console.error('ReactMaterial Error: Button attr - type is empty or invalid \nComponent will render abnormal ui.');
         }
+
+        let btnSize;
+        if (this.state.options.size === 'normal') {} else if (this.state.options.size === 'large') {
+            btnSize = 'btn-large';
+        } else if (this.state.options.size === 'small') {
+            btnSize = 'btn-small';
+        } else if(this.state.options.size !== ' '){
+            console.warn('ReactMaterial Warning: Button attr - size is invalid \n Component will render abnormal ui.');
+        }
+
         return (
             <a
-                className={`waves-effect waves-light ${btnType} ${this.state.options.color} ${this.state.options.textColor}`}>
+                className={`waves-effect waves-light ${btnType} ${btnSize
+                ? btnSize
+                : ''} ${this.state.options.color} ${this.state.options.textColor}`}>
                 {this.state.options.label}
                 {this.props.children}
             </a>
