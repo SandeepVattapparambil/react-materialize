4424fcb4dac516ff6cc7e8dc1d46211f2e3a4cb0
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-08 22:14:57 +0530

    pagination wip

---
+++ src/components/app-pagination/Pagination.js
@@ -0,0 +1,58 @@
+/**
+ * Component - Pagination
+ * MaterializeCSS Pagination component
+ * @see {@link https://materializecss.com/pagination.html | Pagination}
+ */
+
+/**
+ * Import react library
+ */
+import React, {Component} from "react";
+
+/**
+ * @class
+ * @name Pagination
+ * @description MaterializeCSS Pagination Component.
+ * @extends Component
+ */
+class Pagination extends Component {
+    constructor(props) {
+        super(props);
+        let config = {
+            ...props
+        };
+        this.state = {
+            options: {
+                totalItems: config.totalItems || 0,
+                initialPage: config.initialPage || 0,
+                maxItems: config.maxItems || 0
+            }
+        }
+    }
+    render() {
+        let listArray = [];
+        for (let i = 1; i < this.state.options.totalItems; i++) {
+            listArray.push(
+                <li key={i} className={`waves-effect ${i == this.state.options.initialPage ? 'active' : ''}`}>
+                    <a href="#!">{i}</a>
+                </li>
+            );
+        }
+        return (
+            <ul className="pagination">
+                <li className="disabled">
+                    <a href="#!">
+                        <i className="material-icons">chevron_left</i>
+                    </a>
+                </li>
+                {listArray}
+                <li className="waves-effect">
+                    <a href="#!">
+                        <i className="material-icons">chevron_right</i>
+                    </a>
+                </li>
+            </ul>
+        );
+    }
+}
+export default Pagination;
