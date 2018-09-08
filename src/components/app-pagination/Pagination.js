89f937ba10bfb0a3068e20f117428e26ed98b65e
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-08 22:15:40 +0530

    format

--- src/components/app-pagination/Pagination.js
+++ src/components/app-pagination/Pagination.js
@@ -7,7 +7,7 @@
 /**
  * Import react library
  */
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 /**
  * @class
@@ -16,43 +16,47 @@ import React, {Component} from "react";
  * @extends Component
  */
 class Pagination extends Component {
-    constructor(props) {
-        super(props);
-        let config = {
-            ...props
-        };
-        this.state = {
-            options: {
-                totalItems: config.totalItems || 0,
-                initialPage: config.initialPage || 0,
-                maxItems: config.maxItems || 0
-            }
-        }
-    }
-    render() {
-        let listArray = [];
-        for (let i = 1; i < this.state.options.totalItems; i++) {
-            listArray.push(
-                <li key={i} className={`waves-effect ${i == this.state.options.initialPage ? 'active' : ''}`}>
-                    <a href="#!">{i}</a>
-                </li>
-            );
-        }
-        return (
-            <ul className="pagination">
-                <li className="disabled">
-                    <a href="#!">
-                        <i className="material-icons">chevron_left</i>
-                    </a>
-                </li>
-                {listArray}
-                <li className="waves-effect">
-                    <a href="#!">
-                        <i className="material-icons">chevron_right</i>
-                    </a>
-                </li>
-            </ul>
-        );
-    }
+   constructor(props) {
+      super(props);
+      let config = {
+         ...props
+      };
+      this.state = {
+         options: {
+            totalItems: config.totalItems || 0,
+            initialPage: config.initialPage || 0,
+            maxItems: config.maxItems || 0
+         }
+      };
+   }
+   render() {
+      let listArray = [];
+      for (let i = 1; i < this.state.options.totalItems; i++) {
+         listArray.push(
+            <li
+               key={i}
+               className={`waves-effect ${
+                  i == this.state.options.initialPage ? "active" : ""
+               }`}>
+               <a href="#!">{i}</a>
+            </li>
+         );
+      }
+      return (
+         <ul className="pagination">
+            <li className="disabled">
+               <a href="#!">
+                  <i className="material-icons">chevron_left</i>
+               </a>
+            </li>
+            {listArray}
+            <li className="waves-effect">
+               <a href="#!">
+                  <i className="material-icons">chevron_right</i>
+               </a>
+            </li>
+         </ul>
+      );
+   }
 }
 export default Pagination;
