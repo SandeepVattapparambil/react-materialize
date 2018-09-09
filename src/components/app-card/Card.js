be589a6cb1cd57391455fd502cce44e977e419f0
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-09 20:25:42 +0530

    card wip

--- src/components/app-card/Card.js
+++ src/components/app-card/Card.js
@@ -30,7 +30,8 @@ class Card extends Component {
          options: {
             bgColor: config.bgColor || " ",
             textColor: config.textColor || " ",
-            title: config.title || " "
+            title: config.title || " ",
+            size: config.size || {}
          }
       };
    }
@@ -38,24 +39,41 @@ class Card extends Component {
    render() {
       let cardContent;
       let cardAction;
+      let cardReveal;
       if (typeof this.props.children === "string") {
          cardContent = this.props.children;
       } else {
          cardContent = this.props.children[0];
          cardAction = this.props.children[1];
+         cardReveal = this.props.children[2];
       }
       return (
          <Row>
-            <Column small="12" medium="6">
+            <Column
+               small={this.state.options.size.small}
+               medium={this.state.options.size.medium}
+               large={this.state.options.size.large}>
                <div className={`card ${this.state.options.bgColor}`}>
-                  <div className={`card-content ${this.state.options.textColor}`}>
+                  <div className="card-image">
+                     <img src="https://picsum.photos/100/100/?random" />
                      <span className="card-title">
+                        {this.state.options.title}
+                     </span>
+                     <a className="btn-floating halfway-fab waves-effect waves-light red">
+                        <Icon icon="add" />
+                     </a>
+                  </div>
+
+                  <div className={`card-content ${this.state.options.textColor}`}>
+                     <span className="card-title activator">
                         <Icon align="left" icon="menu" />
                         {this.state.options.title}
+                        <Icon align="right" icon="more_vert" />
                      </span>
                      <p>{cardContent}</p>
                   </div>
                   {cardAction}
+                  {cardReveal}
                </div>
             </Column>
          </Row>
