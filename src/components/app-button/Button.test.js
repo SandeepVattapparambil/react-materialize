33aecd0fe505c3189493012393633b35334622c5
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-10 13:03:40 +0530

    unit test updates

--- src/components/app-button/Button.test.js
+++ src/components/app-button/Button.test.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from './Button';
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./Button";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Button
-    type="raised"
-    size="large"
-    disabled={false}
-    label="Click"
-    color="lime"
-    textColor="white-text"
-    wavesEffect={true}
-    wavesType="regular"
-    wavesColor="waves-red"
-    href="http://www.sandeepv.in"
-    onClick={() => {
-       alert("hello");
-    }}>
- </Button>, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+   const div = document.createElement("div");
+   ReactDOM.render(
+      <Button
+         type="raised"
+         size="large"
+         disabled={false}
+         label="Click"
+         color="lime"
+         textColor="white-text"
+         wavesEffect={true}
+         wavesType="regular"
+         wavesColor="waves-red"
+         href="http://www.sandeepv.in"
+         onClick={() => {
+            alert("hello");
+         }}
+      />,
+      div
+   );
+
+   ReactDOM.render(
+      <Button
+         type="floating"
+         size="large"
+         disabled={false}
+         color="blue"
+         textColor="white-text"
+         wavesEffect={true}
+         wavesType="regular"
+         wavesColor="waves-red"
+         pulse={true}>
+         <Icon icon="edit" />
+      </Button>,
+      div
+   );
+
+   ReactDOM.unmountComponentAtNode(div);
 });
