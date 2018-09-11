1248c68948fda78d4d4f341ad27e50c963435fb9
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-11 13:19:00 +0530

    cleanups

--- src/components/app-collection/Collection.js
+++ src/components/app-collection/Collection.js
@@ -37,42 +37,6 @@ class Collection extends Component {
                ""
             )}
             {this.props.children}
-            {/* <li className="collection-item">Alvin</li>
-            <li className="collection-item">
-               <div>
-                  Alvin
-                  <a href="#!" className="secondary-content">
-                     <i className="material-icons">send</i>
-                  </a>
-               </div>
-            </li>
-            <li className="collection-item avatar">
-               <img
-                  src="https://materializecss.com/images/yuna.jpg"
-                  alt=""
-                  className="circle"
-               />
-               <span className="title">Title</span>
-               <p>
-                  First Line <br />
-                  Second Line
-               </p>
-               <a href="#!" className="secondary-content">
-                  <i className="material-icons">grade</i>
-               </a>
-            </li>
-            <li className="collection-item">Alvin</li>
-            <li className="collection-item avatar">
-               <i className="material-icons circle green">insert_chart</i>
-               <span className="title">Title</span>
-               <p>
-                  First Line <br />
-                  Second Line
-               </p>
-               <a href="#!" className="secondary-content">
-                  <i className="material-icons">grade</i>
-               </a>
-            </li> */}
          </ul>
       );
    }
