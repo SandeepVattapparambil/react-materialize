f5c7eff5f9bc1ca7656660defdc4dd2b2bd19ea8
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-13 14:09:43 +0530

    updated

--- src/components/app-carousel/Carousel.js
+++ src/components/app-carousel/Carousel.js
@@ -33,15 +33,25 @@ class Carousel extends Component {
             carouselOptions: config.carouselOptions || {},
             imageData: config.imageData || [],
             textData: config.textData || [],
-            button: config.button || " "
+            button: config.button || " ",
+            componentId: this._generateComponentId()
          }
       };
-      this.elements;
-      this.instances;
+      this.element;
+      this.instance;
    }
 
+   /**
+    * A helper function to generate a unique identifier for component
+    */
+   _generateComponentId = () => {
+      return Math.random()
+         .toString(36)
+         .substring(7);
+   };
+
    componentDidMount() {
-      this.elements = document.querySelectorAll(".carousel");
+      this.element = document.querySelector(`#${this.state.options.componentId}`);
       //Defaults
       let options = {
          duration: this.state.options.carouselOptions.duration || 200,
@@ -61,7 +71,7 @@ class Carousel extends Component {
          noWrap: this.state.options.carouselOptions.noWrap || false,
          onCycleTo: this.state.options.carouselOptions.onCycleTo || null
       };
-      this.instances = M.Carousel.init(this.elements, options);
+      this.instance = M.Carousel.init(this.element, options);
    }
 
    //TODO
@@ -69,96 +79,97 @@ class Carousel extends Component {
    /**
     * A helper function to get the single instance of carousel
     */
-//    _getInstance = element => {};
+   //    _getInstance = element => {};
 
-//    /**
-//     * @function
-//     * Move carousel to next slide or go forward a given amount of slides.
-//     * @inner
-//     * @name _next
-//     * @memberof Carousel
-//     * @param {Number} n - How many times the carousel slides.
-//     */
-//    _next = n => {
-//       this.instances.next(n);
-//    };
+   //    /**
+   //     * @function
+   //     * Move carousel to next slide or go forward a given amount of slides.
+   //     * @inner
+   //     * @name _next
+   //     * @memberof Carousel
+   //     * @param {Number} n - How many times the carousel slides.
+   //     */
+   //    _next = n => {
+   //       this.instances.next(n);
+   //    };
 
-//    /**
-//     * @function
-//     * Move carousel to previous slide or go back a given amount of slides.
-//     * @inner
-//     * @name _prev
-//     * @memberof Carousel
-//     * @param {Number} n - How many times the carousel slides.
-//     */
-//    _prev = n => {
-//       this.instances.prev(n);
-//    };
+   //    /**
+   //     * @function
+   //     * Move carousel to previous slide or go back a given amount of slides.
+   //     * @inner
+   //     * @name _prev
+   //     * @memberof Carousel
+   //     * @param {Number} n - How many times the carousel slides.
+   //     */
+   //    _prev = n => {
+   //       this.instances.prev(n);
+   //    };
 
-//    /**
-//     * @function
-//     * Move carousel to nth slide
-//     * @inner
-//     * @name _set
-//     * @memberof Carousel
-//     * @param {Number} i - Index of slide.
-//     */
-//    _set = i => {
-//       this.instances.set(i);
-//    };
+   //    /**
+   //     * @function
+   //     * Move carousel to nth slide
+   //     * @inner
+   //     * @name _set
+   //     * @memberof Carousel
+   //     * @param {Number} i - Index of slide.
+   //     */
+   //    _set = i => {
+   //       this.instances.set(i);
+   //    };
 
-//    /**
-//     * @function
-//     * Destroy plugin instance and teardown.
-//     * @inner
-//     * @name _destroy
-//     * @memberof Carousel
-//     */
-//    _destroy = () => {
-//       this.instances.destroy();
-//    };
+   //    /**
+   //     * @function
+   //     * Destroy plugin instance and teardown.
+   //     * @inner
+   //     * @name _destroy
+   //     * @memberof Carousel
+   //     */
+   //    _destroy = () => {
+   //       this.instances.destroy();
+   //    };
 
-//    /**
-//     * A helper function to get options the instance was initialized with.
-//     */
-//    _getInstanceOptions = () => {
-//       return this.elements.options;
-//    };
+   //    /**
+   //     * A helper function to get options the instance was initialized with.
+   //     */
+   //    _getInstanceOptions = () => {
+   //       return this.elements.options;
+   //    };
 
-//    /**
-//     * A helper function to get the element on which the carousel is initialized
-//     */
-//    _getEl = () => {
-//       return this.elements.el;
-//    };
+   //    /**
+   //     * A helper function to get the element on which the carousel is initialized
+   //     */
+   //    _getEl = () => {
+   //       return this.elements.el;
+   //    };
 
-//    /**
-//     * A helper function to check whether the carousel is pressed
-//     */
-//    _isPressed = () => {
-//       return this.elements.pressed;
-//    };
+   //    /**
+   //     * A helper function to check whether the carousel is pressed
+   //     */
+   //    _isPressed = () => {
+   //       return this.elements.pressed;
+   //    };
 
-//    /**
-//     * A helper function to check whether the carousel is dragged
-//     */
-//    _isDragged = () => {
-//       return this.elements.dragged;
-//    };
+   //    /**
+   //     * A helper function to check whether the carousel is dragged
+   //     */
+   //    _isDragged = () => {
+   //       return this.elements.dragged;
+   //    };
 
-//    /**
-//     * A helper function to get the index of the current center item of the carousel
-//     */
-//    _getCenter = () => {
-//       return this.elements.center;
-//    };
-///////////////////////////////////////////////////////////////////////////////////////////////
+   //    /**
+   //     * A helper function to get the index of the current center item of the carousel
+   //     */
+   //    _getCenter = () => {
+   //       return this.elements.center;
+   //    };
+   ///////////////////////////////////////////////////////////////////////////////////////////////
    /**
     * Render the carousel
     */
    render() {
       return (
          <div
+            id={this.state.options.componentId}
             className={`carousel ${this.state.options.type} ${
                this.state.options.textData ? "center" : ""
             }`}>
