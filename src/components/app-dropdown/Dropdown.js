8a63ffec94ca96ea699f09884bebc2826b10f920
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-14 20:35:05 +0530

    dropdown wip

--- src/components/app-dropdown/Dropdown.js
+++ src/components/app-dropdown/Dropdown.js
@@ -10,52 +10,72 @@
 import React, { Component, Fragment } from "react";
 
 /**
+ * Import components
+ */
+import Button from "../app-button/Button";
+
+/**
  * @class
  * @name Dropdown
  * @description MaterializeCSS Dropdown Component.
  * @extends Component
  */
 class Dropdown extends Component {
-    constructor(props) {
-        super(props);
-        let config = {...props};
-        this.state = {
-            options:{}
-        };
-        this.element;
-        this.instance;
-    }
+   constructor(props) {
+      super(props);
+      let config = { ...props };
+      this.state = {
+         options: {
+             componentId: this._generateComponentId()
+         }
+      }
+      this.element;
+      this.instance;
+   }
+
+   componentDidMount() {
+      this.element = document.querySelector(".dropdown-content");
+      let options = {
+         alignment: this.state.options.alignment || "left",
+         autoTrigger: this.state.options.autoTrigger || true,
+         constrainWidth: this.state.options.constrainWidth || true,
+         container: this.state.options.container || null,
+         coverTrigger: this.state.options.coverTrigger || true,
+         closeOnClick: this.state.options.closeOnClick || true,
+         hover: this.state.options.hover || false,
+         inDuration: this.state.options.inDuration || 150,
+         outDuration: this.state.options.outDuration || 250,
+         onOpenStart: this.state.options.onOpenStart || (() => {}),
+         onOpenEnd: this.state.options.onOpenEnd || (() => {}),
+         onCloseStart: this.state.options.onCloseStart || (() => {}),
+         onCloseEnd: this.state.options.onCloseEnd || (() => {})
+      };
+      this.instance = M.Dropdown.init(this.element);
+   }
+
+   /**
+    * @function
+    * A helper function to generate a unique identifier for component
+    * @name _generateComponentId
+    * @returns {String} - A unique random string
+    */
+   _generateComponentId = () => {
+      return Math.random()
+         .toString(36)
+         .substring(7);
+   };
+
    render() {
       return (
          <Fragment>
-            <a className="dropdown-trigger btn" href="#" data-target="dropdown1">
-               Drop Me!
-            </a>
-
-            <ul id="dropdown1" className="dropdown-content">
-               <li>
-                  <a href="#!">one</a>
-               </li>
-               <li>
-                  <a href="#!">two</a>
-               </li>
-               <li className="divider" tabIndex="-1" />
-               <li>
-                  <a href="#!">three</a>
-               </li>
-               <li>
-                  <a href="#!">
-                     <i className="material-icons">view_module</i>
-                     four
-                  </a>
-               </li>
-               <li>
-                  <a href="#!">
-                     <i className="material-icons">cloud</i>
-                     five
-                  </a>
-               </li>
-            </ul>
+            <Button
+               type="raised"
+               color="teal"
+               href="#"
+               trigger="dropdown-trigger"
+               dataTarget="dropdown1"
+               label="Drop Me!"
+            />
          </Fragment>
       );
    }
