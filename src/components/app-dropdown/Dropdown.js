8c789149462d4ec0b1f6c2e9c7a5e92bf5b9edb6
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-14 21:01:19 +0530

    formatting

--- src/components/app-dropdown/Dropdown.js
+++ src/components/app-dropdown/Dropdown.js
@@ -26,9 +26,9 @@ class Dropdown extends Component {
       let config = { ...props };
       this.state = {
          options: {
-             componentId: this._generateComponentId()
+            componentId: this._generateComponentId()
          }
-      }
+      };
       this.element;
       this.instance;
    }
@@ -76,6 +76,29 @@ class Dropdown extends Component {
                dataTarget="dropdown1"
                label="Drop Me!"
             />
+            <ul id="dropdown1" className="dropdown-content">
+               <li>
+                  <a href="#!">one</a>
+               </li>
+               <li>
+                  <a href="#!">two</a>
+               </li>
+               <li>
+                  <a href="#!">three</a>
+               </li>
+               <li>
+                  <a href="#!">
+                     <i className="material-icons">view_module</i>
+                     four
+                  </a>
+               </li>
+               <li>
+                  <a href="#!">
+                     <i className="material-icons">cloud</i>
+                     five
+                  </a>
+               </li>
+            </ul>
          </Fragment>
       );
    }
