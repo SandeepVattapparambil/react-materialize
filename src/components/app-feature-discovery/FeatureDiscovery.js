0b64b4bfaff771981665c185ef5bb4e0cc769b65
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-16 20:20:43 +0530

    stable

--- src/components/app-feature-discovery/FeatureDiscovery.js
+++ src/components/app-feature-discovery/FeatureDiscovery.js
@@ -25,23 +25,51 @@ class FeatureDiscovery extends Component {
       super(props);
       let config = { ...props };
       this.state = {
-         options: {}
+         options: {
+            buttonType: config.buttonType || " ",
+            buttonSize: config.buttonSize || " ",
+            buttonColor: config.buttonColor || " ",
+            wavesEffect: config.wavesEffect || true,
+            wavesColor: config.wavesColor || " ",
+            overlayColor: config.overlayColor || " ",
+            icon: config.icon || " ",
+            label: config.label || " ",
+            componentId: this._generateComponentId()
+         }
       };
       this.element;
       this.instance;
    }
 
    componentDidMount() {
-      this.element = document.querySelector(".tap-target");
+      this.element = document.querySelector(
+         `.tapTarget${this.state.options.componentId}`
+      );
       this.instance = M.TapTarget.init(this.element);
    }
 
-   open = () => {
+   _open = () => {
       this.instance.open();
    };
 
-   close = () => {
+   _close = () => {
       this.instance.close();
+   };
+
+   _destroy = () => {
+      this.instance.destroy();
+   };
+
+   /**
+    * @function
+    * A helper function to generate a unique identifier for component
+    * @name _generateComponentId
+    * @returns {String} - A unique random string
+    */
+   _generateComponentId = () => {
+      return Math.random()
+         .toString(36)
+         .substring(7);
    };
 
    render() {
@@ -49,15 +77,23 @@ class FeatureDiscovery extends Component {
          <Fragment>
             <a
                id="menu"
-               className="waves-effect waves-light btn-floating btn-large lime"
-               onClick={this.open}>
-               <Icon icon="menu" />
+               className={`
+               ${this.state.options.buttonColor} 
+               ${this.state.options.buttonSize} 
+               ${this.state.options.buttonType} 
+               ${this.state.options.wavesEffect} 
+               ${this.state.options.wavesColor}
+              `}
+               onClick={this._open}>
+               <Icon icon={this.state.options.icon} />
+               {this.state.options.label}
             </a>
-            <div className="tap-target lime" data-target="menu">
-               <div className="tap-target-content">
-                  <h5>Title</h5>
-                  <p>A bunch of text</p>
-               </div>
+            <div
+               className={`tapTarget${this.state.options.componentId} tap-target 
+               ${this.state.options.overlayColor}
+               `}
+               data-target="menu">
+               <div className="tap-target-content">{this.props.children}</div>
             </div>
          </Fragment>
       );
