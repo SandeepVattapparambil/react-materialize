9a4c14af10c75c166db58f5c92a9a631f6772364
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-09-18 20:02:06 +0530

    wip

--- src/index.js
+++ src/index.js
@@ -630,7 +630,32 @@ ReactDOM.render(
             imageSrc="https://images.unsplash.com/photo-1537123547273-e59f4f437f1b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6086121ab2201b250edd1054994522d3&auto=format&fit=crop&w=750&q=80"
          /> */}
 
-         <Slider/>
+         {/* <Slider
+            sliderOptions={{ indicators: true }}
+            sliderData={[
+               {
+                  imageUrl:
+                     "https://images.unsplash.com/photo-1537152007401-34abcbf532ce?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=382802ab1e00f0cbdc23d7a6d678f5ad&auto=format&fit=crop&w=1510&q=80",
+                  title: "This is our big Tagline!",
+                  subtitle: "Here's our small slogan.",
+                  textAlign: "center-align"
+               },
+               {
+                  imageUrl:
+                     "https://images.unsplash.com/photo-1537151377170-9c19a791bbea?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=43b83b0d2ac9fc3db2d9afd19de0bd6a&auto=format&fit=crop&w=750&q=80",
+                  title: "This is our big Tagline!",
+                  subtitle: "Here's our small slogan.",
+                  textAlign: "left-align"
+               },
+               {
+                  imageUrl:
+                     "https://images.unsplash.com/photo-1537135086-ca6612c6550b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=600db5d4133321cad811ff2ceeffe885&auto=format&fit=crop&w=751&q=80",
+                  title: "This is our big Tagline!",
+                  subtitle: "Here's our small slogan.",
+                  textAlign: "right-align"
+               }
+            ]}
+         /> */}
       </Row>
    </Container>,
    document.getElementById("container")
